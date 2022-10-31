6f68d674eedef4345ceeba0a5774bfa0e8664a6e
Leonardo Monarini <leonardo.monarini@jazztech.com.br>
2022-10-31 14:38:06 -0300

    ajuste nas validacoes

--- main.tf
+++ main.tf
@@ -51,17 +51,6 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
-  tags {
-    tags = merge(
-      local.common_tags,
-      {
-        Name        = "tg${var.environment}${var.name}"
-        Project     = "${var.name}"
-        Environment = "${var.environment}"
-      }
-    )
-  }
 }
 
 resource "aws_ecs_service" "service" {
